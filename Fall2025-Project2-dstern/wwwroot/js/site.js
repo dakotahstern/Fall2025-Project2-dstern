c29488140a062b566af3f30c9d1c8f191abab291
dakotahstern <codystern9@gmail.com>
2025-12-18 18:02:08 -0500

    Time pop up

--- Fall2025-Project2-dstern/wwwroot/js/site.js
+++ Fall2025-Project2-dstern/wwwroot/js/site.js
@@ -2,3 +2,27 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(function () {
+    $("#bttnTime").on("click", function () {
+        const now = new Date();
+        const time = now.toLocaleTimeString([], {
+            hour: "2-digit",
+            minute: "2-digit",
+            hour12: false
+        });
+
+        $("#time")
+            .text(time)
+            .css("visibility", "visible")
+            .dialog({
+                modal: true,
+                title: "Current Time",
+                buttons: {
+                    Close: function () {
+                        $(this).dialog("close");
+                    }
+                }
+            });
+    });
+});
