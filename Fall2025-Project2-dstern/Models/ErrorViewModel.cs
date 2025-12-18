5925061df9d8fa6c9fe60c1c45b46e5a0cbc9c6f
dakotahstern <codystern9@gmail.com>
2025-12-18 17:24:05 -0500

    Add project files.

---
+++ Fall2025-Project2-dstern/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fall2025_Project2_dstern.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
