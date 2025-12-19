3181e01b6daa8f577a0e0337fc62b2447dc76097
dakotahstern <codystern9@gmail.com>
2025-12-18 19:48:14 -0500

    full styling, last step before load more results

--- Fall2025-Project2-dstern/wwwroot/js/site.js
+++ Fall2025-Project2-dstern/wwwroot/js/site.js
@@ -12,6 +12,9 @@ $(function () {
     const cx = "45aaadabb52d048e0";
 
     let backgrndIndex = 0;
+    let startIndex = 1;
+    let lastQuery = "";
+
 
     const backgrounds = [
         "https://wallup.net/wp-content/uploads/2016/01/13156-threadless-humor-simple-minimalism-computer-basketball-coffee-simple_background.jpg", "https://wallpapers.com/images/featured/simple-desktop-background-irf9eel64ijrae3g.jpg", "https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvcm0xNDctbXludC03MF8xLmpwZw.jpg", "https://marketplace.canva.com/EAFSAsQmQeg/1/0/1600w/canva-orange-illustrated-cute-christmas-house-desktop-wallpaper-IscQ9unhXnA.jpg", "https://wallpapers.com/images/hd/simple-desktop-keyboard-keys-fzpfef8zvxj6jnt7.jpg"
